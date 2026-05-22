83d7e4703ae8193506264b6592015ec39b3a37ee
Camilo Laiton <36769694+camilolaiton@users.noreply.github.com>
2026-05-22 12:33:37 -0700

    updating launch

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -4,10 +4,10 @@ export ANNOTATOR_ROLES_FILE=../configs/roles.json
 export ANNOTATOR_CLASSES_FILE=../configs/classes.json
 
 # S3 config
-export ANNOTATOR_S3_DATA_BUCKET=aind-scratch-data
-export ANNOTATOR_S3_DATA_PREFIX=proteomics_annotations/data
-export ANNOTATOR_S3_OUTPUT_BUCKET=aind-scratch-data
-export ANNOTATOR_S3_OUTPUT_PREFIX=proteomics_annotations/annotations
-export ANNOTATOR_S3_PROFILE=aind_dev_sso
+export ANNOTATOR_S3_DATA_BUCKET=some-bucket
+export ANNOTATOR_S3_DATA_PREFIX=data-prefix
+export ANNOTATOR_S3_OUTPUT_BUCKET=some-bucket
+export ANNOTATOR_S3_OUTPUT_PREFIX=output-prefix
+export ANNOTATOR_S3_PROFILE=your-profile
 
 python -m aind_proteomics_annotator
